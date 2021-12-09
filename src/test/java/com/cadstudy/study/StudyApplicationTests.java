f07b7383961fc093f64889281df3c6b3c079bc9b
unknown <luizaugustotpires@gmail.com>
2021-12-09 13:27:01 -0300

    initial project

---
+++ src/test/java/com/cadstudy/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadstudy.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
